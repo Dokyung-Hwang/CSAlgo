1598018d75aa1fc276d37cc0d5cfc30570b437fa
da9dac <da9dac@gmail.com>
2023-09-29 14:26:46 +0900

    연결 리스트: 백준 5397번

---
+++ src/algorithm/problem/linkedList/P1158.java
@@ -0,0 +1,5 @@
+package algorithm.problem.linkedList;
+
+public interface P1158 {
+	// https://www.acmicpc.net/problem/1158
+}
---
+++ src/algorithm/problem/linkedList/P5397.java
@@ -0,0 +1,5 @@
+package algorithm.problem.linkedList;
+
+public interface P5397 {
+	// https://www.acmicpc.net/problem/5397
+}
