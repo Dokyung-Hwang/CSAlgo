768a3394ee4861dbfcc0bb9361ddbcd361b896cd
da9dac <da9dac@gmail.com>
2023-10-04 22:11:21 +0900

    스택의 활용(수식의 괄호 쌍) 문제 추가

---
+++ src/algorithm/problem/stack/advance/P10799.java
@@ -0,0 +1,5 @@
+package algorithm.problem.stack.advance;
+
+public interface P10799 {
+	// https://www.acmicpc.net/problem/10799
+}
---
+++ src/algorithm/problem/stack/advance/P3986.java
@@ -0,0 +1,5 @@
+package algorithm.problem.stack.advance;
+
+public interface P3986 {
+	// https://www.acmicpc.net/problem/3986
+}
