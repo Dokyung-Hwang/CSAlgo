ad4774514fb6ff74cd492a65ccf15c30b5684f40
kimhaming <kimhamin83@gmail.com>
2023-10-09 16:34:45 +0900

    ✨ feat: BOJ_1475

---
+++ src/algorithm/solution/kimhaming/array/S1475.java
@@ -0,0 +1,34 @@
+package algorithm.solution.kimhaming.array;
+
+import algorithm.problem.array.P1475;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class S1475 implements P1475 {
+    // 벨로그 블로깅해서 첨부하기
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String N = br.readLine();
+        int[] numSet = new int[10];  // N 에 출현하는 0 ~ 9까지의 숫자의 갯수를 저장할 배열
+
+        for (int i = 0; i < N.length(); i++) {
+            numSet[N.charAt(i) - '0']++;    // N 에 출현하는 숫자의 갯수를 저장
+        }
+        // 6과 9에 대한 경우의 수 구하기
+        int sixAndNine = (numSet[6] + numSet[9] + 1) / 2;   // 6이 3개 9가 2개일 때, 총 3세트가 필요하다. -> (5 + 1) / 2
+
+        // 나머지 숫자에 대한 경우의 수 구하기
+        int maxSet = 0;
+        for (int i = 0; i < 10; i++) {
+            if (i != 6 && i != 9) { // 6과 9가 들어가는 세트 수는 제외하고
+                // 최대 세트수만 필요하기 때문에, 방번호에 가장 여러번 사용된 숫자에 대한 세트수를 maxSet에 저장
+                maxSet = Math.max(maxSet, numSet[i]);
+            }
+        }
+
+        System.out.println(Math.max(maxSet, sixAndNine));
+    }
+}
