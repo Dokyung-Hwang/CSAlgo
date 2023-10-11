f624126391eedc49c2900d587768896b39a3b966
da9dac <da9dac@gmail.com>
2023-10-12 05:01:33 +0900

    :sparkles: feat: [231012|da9dac] S7569

---
+++ src/algorithm/solution/da9dac/bfs/S7569.java
@@ -0,0 +1,30 @@
+package algorithm.solution.da9dac.bfs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import algorithm.problem.bfs.P7569;
+
+public class S7569 implements P7569 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String[] mnh = br.readLine().split(" ");
+
+		int m = Integer.parseInt(mnh[0]);
+		int n = Integer.parseInt(mnh[1]);
+		int h = Integer.parseInt(mnh[2]);
+
+		int[][][] box = new int[h][n][m];
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				for (int k = 0; k < n; k++) {
+
+				}
+			}
+		}
+	}
+}
