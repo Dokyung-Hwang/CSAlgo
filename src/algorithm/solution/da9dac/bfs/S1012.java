0d07bdd425f3b30d0807adabd4385322a6fac250
da9dac <da9dac@gmail.com>
2023-10-11 17:00:50 +0900

    :sparkles: feat: [231011|da9dac] S1012

--- src/algorithm/solution/da9dac/bfs/S1012.java
+++ src/algorithm/solution/da9dac/bfs/S1012.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.Arrays;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
