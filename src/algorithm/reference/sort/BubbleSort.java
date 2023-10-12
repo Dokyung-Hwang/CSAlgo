1be8b1b33965651b3fc2a9baf25adc4975797734
da9dac <da9dac@gmail.com>
2023-10-12 21:33:25 +0900

    ✨ feat: [231012|da9dac] 버블 정렬 구현

--- src/algorithm/reference/sort/BubbleSort.java
+++ src/algorithm/reference/sort/BubbleSort.java
@@ -1,5 +1,7 @@
 package algorithm.reference.sort;
 
+import java.util.Arrays;
+
 public class BubbleSort {
 	/*
 		https://www.youtube.com/watch?v=59fZkZO0Bo4&list=PLtqbFd2VIQv4O6D6l9HcD732hdrnYb6CY&index=15
@@ -12,6 +14,37 @@ public class BubbleSort {
 		이를 계속 반복 하여 정렬 하는 방법이다.
 
 		배열 {2, 13, 6, 4, -2}를 버블 정렬을 이용 해서 정렬 하려면
+		아래와 같은 순서대로 진행 된다.
 
+		[2, 13, 6, 4, -2]
+		[2, 6, 13, 4, -2]
+		[2, 6, 4, 13, -2]
+		[2, 6, 4, -2, 13]
+		[2, 6, 4, -2, 13]
+		[2, 4, 6, -2, 13]
+		[2, 4, -2, 6, 13]
+		[2, 4, -2, 6, 13]
+		[2, -2, 4, 6, 13]
+		[-2, 2, 4, 6, 13]
 	 */
+
+	public static void main(String[] args) {
+		int[] arr = {2, 13, 6, 4, -2};
+		int n = arr.length;
+
+		System.out.println("정렬 전 : " + Arrays.toString(arr));
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n - 1 - i; j++) {
+				if (arr[j] > arr[j + 1]) {
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+				System.out.println(Arrays.toString(arr));
+			}
+		}
+
+		System.out.println("정렬 후 : " + Arrays.toString(arr));
+	}
 }
