2694aaaa5c086a2f827d483dd657f36dc5692799
da9dac <da9dac@gmail.com>
2023-10-14 23:31:05 +0900

    ✨ feat: [231014|da9dac] S1074

---
+++ src/algorithm/solution/da9dac/recursion/S1074.java
@@ -0,0 +1,34 @@
+package algorithm.solution.da9dac.recursion;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+import algorithm.problem.baejoon.recursion.P1074;
+
+public class S1074 implements P1074 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+		int n = Integer.parseInt(st.nextToken());
+		int r = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+
+		System.out.println(func(n, r, c));
+	}
+
+	private static int func(int n, int r, int c) {
+		if (n == 0) return 0;
+
+		int half = (int) Math.pow(2, n - 1);
+
+		if(r < half && c < half) return func(n-1, r, c);
+		if(r < half && c >= half) return half*half + func(n-1, r, c-half);
+		if(r >= half && c < half) return 2*half*half + func(n-1, r-half, c);
+		return 3*half*half + func(n-1, r-half, c-half);
+	}
+}
