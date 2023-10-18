3f0df41e11a8778ce7185cbd86c4aa9c2bb0ca13
dohyoungK <gdh012@pusan.ac.kr>
2023-10-18 18:57:55 +0900

    ✨	feat: programmers 스택/큐 문제

---
+++ src/algorithm/problem/programmers/stack_queue/P1.java
@@ -0,0 +1,6 @@
+package algorithm.problem.programmers.stack_queue;
+
+public interface P1 {
+    // 같은 숫자는 싫어
+    // https://school.programmers.co.kr/learn/courses/30/lessons/12906
+}
---
+++ src/algorithm/problem/programmers/stack_queue/P4.java
@@ -0,0 +1,6 @@
+package algorithm.problem.programmers.stack_queue;
+
+public interface P4 {
+    // 프로세스
+    // https://school.programmers.co.kr/learn/courses/30/lessons/42587
+}
