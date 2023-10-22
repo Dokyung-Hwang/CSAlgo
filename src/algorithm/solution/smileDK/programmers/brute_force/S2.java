c67e83054e1ecc8208615b524bcebecf6e335756
Smile:DK <dokyung226@gmail.com>
2023-10-22 21:37:13 +0900

    ♻️[20231022|SmileDK] Programmers - [완전 탐색] 모의 고사, 브랜치명 변경

--- src/algorithm/solution/smileDK/programmers/brute_force/S2.java
+++ src/algorithm/solution/smileDK/programmers/brute_force/S2.java
@@ -14,9 +14,9 @@ public class S2 implements P2 {
 
     public static int[] solution(int[] answers) {
         int[] answer = {};
-        int[] person1 = {1,2,3,4,5};                // 수포자 1
-        int[] person2 = {2,1,2,3,2,4,2,5};          // 수포자 2
-        int[] person3 = {3,3,1,1,2,2,4,4,5,5};      // 수포자 3
+        int[] person1 = {1,2,3,4,5};
+        int[] person2 = {2,1,2,3,2,4,2,5};
+        int[] person3 = {3,3,1,1,2,2,4,4,5,5};
 
         int answer1=0, answer2 =0, answer3 =0;
 
@@ -25,9 +25,9 @@ public class S2 implements P2 {
             if(person2[i%person2.length] == answers[i]) answer2++;
             if(person3[i%person3.length] == answers[i]) answer3++;
         }
-        int max = Math.max(Math.max(answer1, answer2),answer3); // max 값 구하기
+        int max = Math.max(Math.max(answer1, answer2),answer3);
         ArrayList<Integer> list = new ArrayList<Integer>();
-        if(max==answer1) list.add(1); //max 값 이랑 같으면 넣는다.
+        if(max==answer1) list.add(1);
         if(max==answer2) list.add(2);
         if(max==answer3) list.add(3);
 
