b8c77254f79615a63a56b8f3da2ae94c64652e07
da9dac <da9dac@gmail.com>
2023-10-24 13:23:30 +0900

    ✨ feat: [231024|da9dac] 백트래킹 N과 M 시리즈 문제 추가

---
+++ src/algorithm/problem/baejoon/backTracking/P15650.java
@@ -0,0 +1,5 @@
+package algorithm.problem.baejoon.backTracking;
+
+public interface P15650 {
+	// https://www.acmicpc.net/problem/15650
+}
---
+++ src/algorithm/problem/baejoon/backTracking/P15654.java
@@ -0,0 +1,5 @@
+package algorithm.problem.baejoon.backTracking;
+
+public interface P15654 {
+	// https://www.acmicpc.net/problem/15654
+}
