15f948b19948c53b9d8d87ca9e9adf88ff2469e2
da9dac <da9dac@gmail.com>
2023-10-30 16:33:10 +0900

    ✨ feat: [231030|da9dac] 완전탐색 S3

---
+++ src/algorithm/solution/da9dac/ProgrammersRun.java
@@ -0,0 +1,12 @@
+package algorithm.solution.da9dac;
+
+import algorithm.solution.da9dac.exhaustiveSearch.backTracking.S3;
+
+public class ProgrammersRun {
+
+	public static void main(String[] args) {
+		S3 solution = new S3();
+		System.out.println(solution.solution("17"));
+		System.out.println(solution.solution("011"));
+	}
+}
