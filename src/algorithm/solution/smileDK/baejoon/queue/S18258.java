5d04e9c112ecd78330264cadb81a457d4ca2a6e4
Smile:DK <dokyung226@gmail.com>
2023-11-01 02:21:38 +0900

    :sparkles: [20231101 | smiledk] 백준 - 18258

--- src/algorithm/solution/smileDK/baejoon/queue/S18258.java
+++ src/algorithm/solution/smileDK/baejoon/queue/S18258.java
@@ -1,6 +1,6 @@
 package algorithm.solution.smileDK.baejoon.queue;
 
-import algorithm.problem.baekjoon.queue.P10845;
+import algorithm.problem.baekjoon.queue.P18258;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -8,15 +8,7 @@ import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.StringTokenizer;
 
-
-/*Todo:
-   push X: 정수 X를 큐에 넣는 연산이다.
-   pop: 큐에서 가장 앞에 있는 정수를 빼고, 그 수를 출력한다. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
-   size: 큐에 들어있는 정수의 개수를 출력한다.
-   empty: 큐가 비어있으면 1, 아니면 0을 출력한다.
-   front: 큐의 가장 앞에 있는 정수를 출력한다. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
-   back: 큐의 가장 뒤에 있는 정수를 출력한다. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.*/
-public class S10845 implements P10845 {
+public class S18258 implements P18258 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -86,4 +78,5 @@ public class S10845 implements P10845 {
         if (queue.isEmpty()) return -1;
         else return queue.get(queue.size() - 1);
     }
+
 }
