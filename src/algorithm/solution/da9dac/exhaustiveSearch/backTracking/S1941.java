7dbcf7f1fa163c817956931e34427d6c5ca95054
da9dac <da9dac@gmail.com>
2023-11-07 00:32:16 +0900

    ✨ feat: [231107|da9dac] 백트래킹 S1941

--- src/algorithm/solution/da9dac/exhaustiveSearch/backTracking/S1941.java
+++ src/algorithm/solution/da9dac/exhaustiveSearch/backTracking/S1941.java
@@ -4,25 +4,17 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.Arrays;
 import java.util.Queue;
 
 import algorithm.problem.baekjoon.backTracking.P1941;
 
 public class S1941 implements P1941 {
-
-	/*
-		S는 이다'솜'파	Y는 임도'연'파
-
-		25명에서 7명을 뽑아야 함
-		7명의 자리는 가로나 세로로 반드시 인접해 있어야 함
-		7명에서 4명 이상은 이다솜파(S)여야 함 (S >= 4)
-	 */
+	// https://da9dac.tistory.com/236
 
 	private static int[] dx = {1, -1, 0, 0};
 	private static int[] dy = {0, 0, 1, -1};
 	private static char[][] seats = new char[5][5];
-	private static boolean[][] isVisited = new boolean[5][5];
+	private static Pair[] seven = new Pair[7];
 	private static Queue<Pair> queue = new ArrayDeque<>();
 	private static int count = 0;
 
@@ -33,14 +25,44 @@ public class S1941 implements P1941 {
 			seats[i] = br.readLine().toCharArray();
 		}
 
+		combination(0, 0, 0);
+
 		System.out.println(count);
 	}
 
-	private static void combination(int size) {
+	private static void combination(int size, int start, int yeon) {
+		if (yeon > 3) return;
+
 		if (size == 7) {
-			count++;
+			if (isLinked()) {
+				count++;
+			}
 			return;
 		}
+
+		for (int i = start; i < 25; i++) {
+			Pair pair = new Pair(i / 5, i % 5);
+			seven[size] = pair;
+			if (seats[pair.x][pair.y] == 'Y') {
+				combination(size + 1, i + 1, yeon + 1);
+			} else {
+				combination(size + 1, i + 1, yeon);
+			}
+		}
+	}
+
+	private static boolean isLinked() {
+		int[][] newSeats = new int[5][5];
+		boolean[][] isVisited = new boolean[5][5];
+		int s = 1;
+
+		for (Pair pair : seven) {
+			newSeats[pair.x][pair.y] = 1;
+		}
+
+		Pair pair = seven[0];
+		queue.offer(pair);
+		isVisited[pair.x][pair.y] = true;
 
 		while (!queue.isEmpty()) {
 			Pair cur = queue.poll();
@@ -48,16 +70,27 @@ public class S1941 implements P1941 {
 			for (int xy = 0; xy < 4; xy++) {
 				int x = cur.x + dx[xy];
 				int y = cur.y + dy[xy];
+
+				if (x < 0 || x > 4 || y < 0 || y > 4) continue;
+				if (isVisited[x][y] || newSeats[x][y] == 0) continue;
+
+				s++;
+				queue.offer(new Pair(x, y));
+				isVisited[x][y] = true;
 			}
+
 		}
+
+		return s == 7;
 	}
 
 	private static class Pair {
 		int x;
 		int y;
 
-		public Pair(int x) {
+		public Pair(int x, int y) {
 			this.x = x;
+			this.y = y;
 		}
 	}
 }
