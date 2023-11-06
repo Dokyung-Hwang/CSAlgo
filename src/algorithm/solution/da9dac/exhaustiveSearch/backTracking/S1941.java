65adf11aa8d8e6150bd06e2d50571a630fa4e3e2
da9dac <da9dac@gmail.com>
2023-11-06 18:31:06 +0900

    ✨ feat: [231106|da9dac] 백트래킹 S1941

--- src/algorithm/solution/da9dac/exhaustiveSearch/backTracking/S1941.java
+++ src/algorithm/solution/da9dac/exhaustiveSearch/backTracking/S1941.java
@@ -3,36 +3,61 @@ package algorithm.solution.da9dac.exhaustiveSearch.backTracking;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
-import java.util.StringTokenizer;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Queue;
 
 import algorithm.problem.baekjoon.backTracking.P1941;
 
 public class S1941 implements P1941 {
 
+	/*
+		S는 이다'솜'파	Y는 임도'연'파
+
+		25명에서 7명을 뽑아야 함
+		7명의 자리는 가로나 세로로 반드시 인접해 있어야 함
+		7명에서 4명 이상은 이다솜파(S)여야 함 (S >= 4)
+	 */
+
+	private static int[] dx = {1, -1, 0, 0};
+	private static int[] dy = {0, 0, 1, -1};
+	private static char[][] seats = new char[5][5];
+	private static boolean[][] isVisited = new boolean[5][5];
+	private static Queue<Pair> queue = new ArrayDeque<>();
+	private static int count = 0;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 
-		char[] expression = br.readLine().toCharArray();
-		boolean isOpen = false;
-
-		for (int i = 0; i < expression.length; i++) {
-			char exp = expression[i];
-			sb.append(exp);
-			if (exp == '-' && !isOpen) {
-					isOpen = !isOpen;
-					sb.append("(");
-			} else if (exp == '-' || (i == expression.length - 1 && isOpen)) {
-				isOpen = !isOpen;
-				sb.append(")");
-			}
+		for (int i = 0; i < 5; i++) {
+			seats[i] = br.readLine().toCharArray();
 		}
 
-		String[] results = sb.toString().split("-");
+		System.out.println(count);
+	}
 
-		// 55 - (50+40)
+	private static void combination(int size) {
+		if (size == 7) {
+			count++;
+			return;
+		}
 
-		System.out.println(sb);
+		while (!queue.isEmpty()) {
+			Pair cur = queue.poll();
+
+			for (int xy = 0; xy < 4; xy++) {
+				int x = cur.x + dx[xy];
+				int y = cur.y + dy[xy];
+			}
+		}
+	}
+
+	private static class Pair {
+		int x;
+		int y;
+
+		public Pair(int x) {
+			this.x = x;
+		}
 	}
 }
