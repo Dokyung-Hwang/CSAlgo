22a7bc5b3d2923e57f37bbe410a569fdb0c8c2ad
da9dac <da9dac@gmail.com>
2023-11-08 19:30:42 +0900

    ✨ feat: [231108|da9dac] BFS S2206

--- src/algorithm/solution/da9dac/Notepad.java
+++ src/algorithm/solution/da9dac/Notepad.java
@@ -3,6 +3,7 @@ package algorithm.solution.da9dac;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 import java.util.Arrays;
 
 public class Notepad {
@@ -11,24 +12,12 @@ public class Notepad {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 
-		int n = Integer.parseInt(br.readLine());
-		int[] arr = new int[2000002];
-		int max = -1111111;
+		BigInteger a = new BigInteger(br.readLine());
+		BigInteger b = new BigInteger(br.readLine());
 
-		for (int i = 0; i < n; i++) {
-			int num = Integer.parseInt(br.readLine());
-			arr[num + 1000001]++;
-			max = Math.max(num, max);
-		}
-
-		for (int i = 1; i <= max + 1000001; i++) {
-			int x = arr[i];
-			if (x != 0) {
-				for (int j = 0; j < x; j++) {
-					sb.append(i - 1000001).append("\n");
-				}
-			}
-		}
+		sb.append(a.add(b)).append("\n");
+		sb.append(a.subtract(b)).append("\n");
+		sb.append(a.multiply(b)).append("\n");
 
 		System.out.println(sb);
 	}
