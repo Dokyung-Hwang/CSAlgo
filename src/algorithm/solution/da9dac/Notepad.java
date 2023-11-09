c75f99c96184d3e49f5ef3b7254c972408694be5
da9dac <da9dac@gmail.com>
2023-11-09 15:22:45 +0900

    ✨ feat: [231109|da9dac] 메모장

--- src/algorithm/solution/da9dac/Notepad.java
+++ src/algorithm/solution/da9dac/Notepad.java
@@ -3,22 +3,15 @@ package algorithm.solution.da9dac;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
-import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Notepad {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		StringBuilder sb = new StringBuilder();
 
-		BigInteger a = new BigInteger(br.readLine());
-		BigInteger b = new BigInteger(br.readLine());
 
-		sb.append(a.add(b)).append("\n");
-		sb.append(a.subtract(b)).append("\n");
-		sb.append(a.multiply(b)).append("\n");
-
-		System.out.println(sb);
 	}
 }
