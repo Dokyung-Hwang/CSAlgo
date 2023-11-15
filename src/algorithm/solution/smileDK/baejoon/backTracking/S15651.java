2affb77fda023652ddf7898084940d9b3e2434be
Smile:DK <dokyung226@gmail.com>
2023-11-15 18:15:30 +0900

    :sparkles: [20231115 | smiledk] BOJ(Back Tracking) - S15651

--- src/algorithm/solution/smileDK/baejoon/backTracking/S15651.java
+++ src/algorithm/solution/smileDK/baejoon/backTracking/S15651.java
@@ -1,45 +1,45 @@
 package algorithm.solution.smileDK.baejoon.backTracking;
 
-import algorithm.problem.baekjoon.backTracking.P15650;
+import algorithm.problem.baekjoon.backTracking.P15651;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class S15650 implements P15650 {
+public class S15651 implements P15651 {
+
+
+    private static int[] sequence;
     private static int n;
     private static int m;
-    private static int[] sequence;
-    private static final StringBuilder sb = new StringBuilder();
+    private static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
         sequence = new int[m];
-
-        dfs(1, 0);
-
+        dfs(0);
         System.out.println(sb);
     }
 
-    // at: 현재 위치 -> 어디서 부터 시작하는 지
-    public static void dfs(int at, int depth) {
+    public static void dfs(int depth) {
         if (depth == m) {
-            for (int value : sequence) {
-                sb.append(value).append(' ');
+            for (int i = 0; i < m; i++) {
+                sb.append(sequence[i]).append(' ');
             }
             sb.append('\n');
             return;
         }
 
-        for (int i = at; i <= n; i++) {
+        for (int i = 1; i <= n; i++) {
             sequence[depth] = i;
-            dfs(i + 1, depth + 1);
+            dfs(depth + 1);
         }
     }
 }
