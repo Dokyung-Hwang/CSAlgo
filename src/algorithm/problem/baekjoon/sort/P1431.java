ba9306a7abe8b640127e7e050599a46ef5d52471
da9dac <da9dac@gmail.com>
2023-11-15 22:32:30 +0900

    ✨ feat: [231115|da9dac] Sort 문제 추가

---
+++ src/algorithm/problem/baekjoon/sort/P1431.java
@@ -0,0 +1,5 @@
+package algorithm.problem.baekjoon.sort;
+
+public interface P1431 {
+	// https://www.acmicpc.net/problem/1431
+}
---
+++ src/algorithm/problem/baekjoon/sort/P7795.java
@@ -0,0 +1,5 @@
+package algorithm.problem.baekjoon.sort;
+
+public interface P7795 {
+	// https://www.acmicpc.net/problem/7795
+}
