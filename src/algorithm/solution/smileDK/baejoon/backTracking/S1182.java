b4bdb3e3a14593804d027b8cf8b28a036a50e791
Smile:DK <dokyung226@gmail.com>
2023-11-20 22:42:51 +0900

    :sparkles: [20231120 | smiledk] BOJ(Sort) - S11728

---
+++ src/algorithm/solution/smileDK/baejoon/backTracking/S1182.java
@@ -0,0 +1,26 @@
+package algorithm.solution.smileDK.baejoon.backTracking;
+
+import algorithm.problem.baekjoon.backTracking.P1182;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class S1182 implements P1182 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+
+    }
+
+    public static void dfs() {
+
+    }
+}
