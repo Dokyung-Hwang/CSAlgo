70333942340fb4b25b7f68144ee6750de5a3f0e8
dohyoungK <gdh012@pusan.ac.kr>
2023-12-14 17:24:01 +0900

    ✨	feat: [231214|dohyoungK] twoPointer 문제 추가

---
+++ src/algorithm/problem/baekjoon/twoPointer/P1806.java
@@ -0,0 +1,5 @@
+package algorithm.problem.baekjoon.twoPointer;
+
+public interface P1806 {
+//    https://www.acmicpc.net/problem/1806
+}
---
+++ src/algorithm/problem/baekjoon/twoPointer/P2230.java
@@ -0,0 +1,5 @@
+package algorithm.problem.baekjoon.twoPointer;
+
+public interface P2230 {
+//    https://www.acmicpc.net/problem/2230
+}
