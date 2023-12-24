bf9fa3bd8a3144d9528271b69bb6fb1119502da0
da9dac <da9dac@gmail.com>
2023-12-24 10:21:04 +0900

    ✨ feat: [231224|da9dac] 그리디 문제 추가

---
+++ src/algorithm/problem/baekjoon/greedy/P1700.java
@@ -0,0 +1,5 @@
+package algorithm.problem.baekjoon.greedy;
+
+public interface P1700 {
+	// https://www.acmicpc.net/problem/1700
+}
---
+++ src/algorithm/problem/baekjoon/greedy/P2217.java
@@ -0,0 +1,5 @@
+package algorithm.problem.baekjoon.greedy;
+
+public interface P2217 {
+	// https://www.acmicpc.net/problem/2217
+}
