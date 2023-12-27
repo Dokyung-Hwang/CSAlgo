76090ea22ad2d5638664c77bd273f6f60380c986
Smile:DK <dokyung226@gmail.com>
2023-12-27 16:15:42 +0900

    ✨ Feat: [20231227 | smiledk] BOJ(dp) - S2579

--- src/algorithm/solution/smileDK/baejoon/dp/S9095.java
+++ src/algorithm/solution/smileDK/baejoon/dp/S9095.java
@@ -6,6 +6,18 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/*Todo:
+   * Operator
+   d[i] = i를 1,2,3의 합으로 나타내는 방법의 수
+   * Recurrence
+   d[4] = ?
+   1+1+1+(1) | 2+1+(1) | 1+2+(1) | 3+(1) => 3을 1,2,3의 합으로 나타내는 방법 + 1, d[3]
+   1+1+(2) | 2+(2) => 2를 1,2,3의 합으로 나타내는 방법 + 2, d[2]
+   1+(3) => 1을 1,2,3의 합으로 나타내는 방법 + 3, d[1]
+   d[4] = d[1] + d[2] + d[3]
+   d[i] = d[i-1] + d[i-2] + d[i-3]
+ */
+
 public class S9095 implements P9095 {
     private static int[] d = new int[11];
 
