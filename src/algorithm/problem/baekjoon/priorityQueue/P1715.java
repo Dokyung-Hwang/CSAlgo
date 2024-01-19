84893072cb6f93595678ef8c331246c0a5de95ea
da9dac <da9dac@gmail.com>
2024-01-19 15:48:00 +0900

    ✨ feat: [240119|da9dac] PriorityQ 문제 추가

---
+++ src/algorithm/problem/baekjoon/priorityQueue/P1715.java
@@ -0,0 +1,5 @@
+package algorithm.problem.baekjoon.priorityQueue;
+
+public interface P1715 {
+	// https://www.acmicpc.net/problem/1715
+}
---
+++ src/algorithm/problem/baekjoon/priorityQueue/P2075.java
@@ -0,0 +1,5 @@
+package algorithm.problem.baekjoon.priorityQueue;
+
+public interface P2075 {
+	// https://www.acmicpc.net/problem/2075
+}
