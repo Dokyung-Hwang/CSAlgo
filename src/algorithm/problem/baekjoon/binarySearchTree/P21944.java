336f012ac138c1d66757f4123253c490d74bbc9d
da9dac <da9dac@gmail.com>
2024-01-29 22:42:38 +0900

    ✨ feat: [240129|da9dac] BST 문제 추가

---
+++ src/algorithm/problem/baekjoon/binarySearchTree/P21944.java
@@ -0,0 +1,5 @@
+package algorithm.problem.baekjoon.binarySearchTree;
+
+public interface P21944 {
+	// https://www.acmicpc.net/problem/21944
+}
---
+++ src/algorithm/problem/baekjoon/binarySearchTree/P23326.java
@@ -0,0 +1,5 @@
+package algorithm.problem.baekjoon.binarySearchTree;
+
+public interface P23326 {
+	// https://www.acmicpc.net/problem/23326
+}
