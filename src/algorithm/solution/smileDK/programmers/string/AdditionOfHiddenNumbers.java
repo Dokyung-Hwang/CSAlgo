dc451ce490068c4b32e4bec3dc11c56c003f9835
Smile:DK <dokyung226@gmail.com>
2024-06-19 00:39:49 +0900

    ✨ feature : [20240619 | smile:DK] Programmers(String) - 가위바위보, 암호해독, 숨어있는 숫자의 덧셈

---
+++ src/algorithm/solution/smileDK/programmers/string/AdditionOfHiddenNumbers.java
@@ -0,0 +1,21 @@
+package algorithm.solution.smileDK.programmers.string;
+
+public class AdditionOfHiddenNumbers {
+    // https://school.programmers.co.kr/learn/courses/30/lessons/120851
+    public static void main(String[] args) {
+        System.out.println(solution("aAb1B2cC34oOp"));
+        System.out.println(solution("1a2b3c4d123"));
+    }
+
+    public static int solution(String my_string) {
+        int sum = 0;
+
+        for (int i = 0; i < my_string.length(); i++) {
+            char c = my_string.charAt(i);
+            if (Character.isDigit(c)) {
+                sum += Character.getNumericValue(c);
+            }
+        }
+        return sum;
+    }
+}
---
+++ src/algorithm/solution/smileDK/programmers/string/RSP.java
@@ -0,0 +1,31 @@
+package algorithm.solution.smileDK.programmers.string;
+
+public class RSP {
+    // https://school.programmers.co.kr/learn/courses/30/lessons/120839
+    public static void main(String[] args) {
+        System.out.println(solution("2"));
+        System.out.println(solution("205"));
+        System.out.println(solution("520"));
+    }
+
+    public static String solution (String rsp) {
+        StringBuilder sb = new StringBuilder();
+        final String rock = "0";
+        final String scissors = "2";
+        final String paper = "5";
+
+        for (int i = 0; i < rsp.length(); i++) {
+            char c = rsp.charAt(i);
+            if (c == rock.charAt(0))
+                sb.append(paper);
+
+            else if (c == scissors.charAt(0))
+                sb.append(rock);
+
+            else
+                sb.append(scissors);
+        }
+
+        return sb.toString();
+    }
+}
